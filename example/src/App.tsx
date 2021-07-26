e114cd15ae39295ec4b88ad266d12ec95706318d
Alek Angelov <alek.angelov.sk@gmail.com>
2021-07-26 10:36:42 +0200

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from '@zbtsu/combo-suite'
+import '@zbtsu/combo-suite/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
